ec28ad46e3e258f4ef433be929d6788c160732b9
neiomi1 <48922182+neiomi1@users.noreply.github.com>
2019-11-08 19:46:22 +0100

    game init

---
+++ Immortal Journey/src/module-info.java
@@ -0,0 +1,13 @@
+// Calc braucht opens
+module code
+{
+    requires javafx.controls;
+
+    requires javafx.fxml;
+
+    requires transitive javafx.graphics;
+
+    requires java.scripting;
+
+    requires javafx.base;
+}
